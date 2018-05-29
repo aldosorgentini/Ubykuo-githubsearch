a17492a0b24f350c7aa1030e7abee3328da115d7
Aldo Sorgentini <aldo.sorgentini@gmail.com>
2018-05-29 01:45:54 -0300

    initial commit.. tarde pero seguro

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    
+    <top-nav inline-template>
+      <div class="container-fluid">
+        <div class="row header-vacio">
+          <form class="form-inline">
+            <input class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </form>
+        </div>
+      </div>
+    </top-nav>
+    <result inline-template>
+      <div>
+      </div>
+    </result>
+    <footer-contact inline-template>
+      <div> </div>
+    </footer-contact>
   </div>
 </template>
 
@@ -19,5 +35,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.header-vacio {
+  text-align: center;
+  vertical-align: 50%;
+  height: 40vh;
+  background: #2828ac;
+  color: #f1ecbd;
+
+}
+.header-vacio h1 {
+  -webkit-text-shadow: 0px 10px 24px -7px rgba(0,0,0,0.75);
+  -moz-text-shadow: 0px 10px 24px -7px rgba(0,0,0,0.75);
+  text-shadow: 0px 10px 24px -7px rgba(0,0,0,0.75);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+fontawesome.library.add(solid.faSearch)
 Vue.config.productionTip = false
+
+Vue.component('top-nav', {
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  data: {
+    resultados: [],
+    buscaor: ''
+
+  },
   components: { App },
   template: '<App/>'
 })
